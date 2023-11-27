ac9f5b3cf5a8f0e1e1d5f14b8ca34baedd0a7569
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-11-27 12:55:45 +0300

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(array):
+    if len(array) == 1:
+        return array[0]
+    elif len(array) == 2:
+        return f"{array[0]} and {array[1]}"
+    else:
+        comma_separated = ", ".join(array[:-1])
+        return f"{comma_separated}, and {array[-1]}"
